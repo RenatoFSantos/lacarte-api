b5de725e3cce6b7f59498b98d1227df2783d55a5
Renato Santos <renato@midilabs.com.br>
2023-08-04 11:49:55 -0300

    Ajuste deploy Cyclic 15

--- src/entity/BaseEntity.ts
+++ src/entity/BaseEntity.ts
@@ -1,6 +1,5 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity({ name: 'BaseEntity' })
 export abstract class BaseEntity {
     @PrimaryGeneratedColumn('uuid')
     uid: string;
